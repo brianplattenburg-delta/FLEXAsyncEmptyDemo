1f8d1ef21d2c093dceb902154185c5074350be13
Brian Plattenburg <brian.plattenburg@delta.com>
2023-05-03 09:05:32 -0400

    Sample code proving FLEX iOS16 not showing response body for async methods

--- FLEXAsyncEmptyDemo/ViewController.swift
+++ FLEXAsyncEmptyDemo/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import FLEX
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var responseDataLabel: UILabel!
+    
+    @IBAction func openFLEXPressed(_ sender: Any) {
+        FLEXManager.shared.showExplorer()
+    }
+    
+    @IBAction func networkRequestPressed(_ sender: Any) {
+        Task {
+            let url = URL(string: "https://cat-fact.herokuapp.com/facts/")
+            guard let url = url else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            do {
+                let (responseData, _) = try await URLSession.shared.data(for: request)
+                let response = try JSONDecoder().decode([CatFact].self, from: responseData)
+                let catFactsString = response.map({ $0.text }).joined(separator: ",\n\n")
+                updateUI(text: catFactsString)
+            } catch let error {
+                updateUI(text: "Error:\n\(error.localizedDescription)")
+            }
+        }
     }
 
-
+    @MainActor func updateUI(text: String) {
+        responseDataLabel.text = text
+    }
 }
 
+struct CatFact: Codable {
+    let text: String // We could map a lot more but we don't really care about whether it maps data back
+}
